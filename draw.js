cf602ed6cf3b25f622c696d9ec12e4ad78ef6f02
Alan Koval <alank399@gmail.com>
2019-06-15 18:56:07 -0500

    Add zoom/pan transformations.

--- draw.js
+++ draw.js
@@ -28,3 +28,28 @@ function snap(){
       }
     );
 }
+
+let scale = 1;
+let translate = new Point(0, 0);
+function graphToScreen(realPoint){
+    return realPoint.scaleBy(scale).add(translate);
+}
+function screenToGraph(screenPt){
+    return screenPt.subtract(translate).scaleBy(1/scale);
+}
+// panBy is a Point in pixels
+function pan(panBy){
+    translate = translate.add(panBy);
+}
+// i.e. center of zoom (in screen coords) and how much of the screen should become the full screen (so .1 is too much zoom)
+function zoom(pixCenter, zoomPercent){
+    let center = screenToGraph(pixCenter);
+    // it is intuitive that scale should be multiplied by 1 / zoomPercent. What should 
+    // happen to translate? 
+    // we wish that the centerpoint not change position, so we have 
+    // scale * center + translate = newScale * center + newTranslate 
+    //   ==> newTranslate = center(scale - newScale) - translate
+    let newScale = scale / zoomPercent;
+    translate = center.scaleBy(scale - newScale).subtract(translate);
+    scale = newScale;
+}
